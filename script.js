75d1cc91be158d78588803131c1d626dd7d63322
Qozeem Salami <qozeemopeyemi2008@gmail.com>
2022-12-15 10:21:50 +0100

    practice modal project

---
+++ script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// modal
+const modal = document.querySelector('.modal');
+
+// overlay hidden
+const overlay = document.querySelector('.overlay');
+
+// close modal
+const btnCloseModal = document.querySelector('.close-modal');
+
+// show modal
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+// key events
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
